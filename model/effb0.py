d73791bb9c1726a5c529dce15dbfadda0b9f0797
KhoiDOO <khoido8899@gmail.com>
2023-06-14 17:54:22 +0700

    EfficienNet has no classifier

--- model/effb0.py
+++ model/effb0.py
@@ -7,7 +7,7 @@ def effb0(weight = True, vector_size = 128):
         weights='IMAGENET1K_V1' if weight else 'DEFAULT'
     )
     out_features = model.classifier[1].in_features
-    del model.classifier
+    # del model.classifier
     model.classifier == nn.Sequential(
         nn.Dropout(0.3),
         nn.Linear(out_features, vector_size, bias=True),
