c3c68a8f76542d60c4159534a01c646ffb116f0a
KhoiDOO <khoido8899@gmail.com>
2023-06-14 17:53:15 +0700

    fix BUG sequential has no in feature

--- model/effb0.py
+++ model/effb0.py
@@ -6,7 +6,7 @@ def effb0(weight = True, vector_size = 128):
     model = models.efficientnet_b0(
         weights='IMAGENET1K_V1' if weight else 'DEFAULT'
     )
-    out_features = model.classifier.in_features
+    out_features = model.classifier[1].in_features
     del model.classifier
     model.classifier == nn.Sequential(
         nn.Dropout(0.3),
