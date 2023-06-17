59228f6cd1a1184c36e389f88741da44700adb5a
KhoiDOO <khoido8899@gmail.com>
2023-06-17 22:56:05 +0700

    update augmentation

---
+++ dataset/augmentation/aug_base.py
@@ -0,0 +1,26 @@
+import os, sys
+import random
+from torchvision import transforms
+from PIL import Image, ImageOps, ImageFilter
+
+class GaussianBlur(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            sigma = random.random() * 1.9 + 0.1
+            return img.filter(ImageFilter.GaussianBlur(sigma))
+        else:
+            return img
+
+
+class Solarization(object):
+    def __init__(self, p):
+        self.p = p
+
+    def __call__(self, img):
+        if random.random() < self.p:
+            return ImageOps.solarize(img)
+        else:
+            return img
---
+++ dataset/augmentation/cls_aug.py
@@ -0,0 +1,48 @@
+import os, sys
+import random
+from torchvision import transforms
+from PIL import Image, ImageOps, ImageFilter
+from .aug_base import *
+import random
+
+class CLSTransform:
+    def __init__(self, size):
+        self.size = size
+        self.transform = transforms.Compose([
+            transforms.RandomResizedCrop(self.size, interpolation=Image.BICUBIC),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4,
+                                        saturation=0.2, hue=0.1)],
+                p=0.8
+            ),
+            transforms.RandomGrayscale(p=0.2),
+            GaussianBlur(p=1.0),
+            Solarization(p=0.0),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
+        self.transform_prime = transforms.Compose([
+            transforms.RandomResizedCrop(self.size, interpolation=Image.BICUBIC),
+            transforms.RandomHorizontalFlip(p=0.5),
+            transforms.RandomApply(
+                [transforms.ColorJitter(brightness=0.4, contrast=0.4,
+                                        saturation=0.2, hue=0.1)],
+                p=0.8
+            ),
+            transforms.RandomGrayscale(p=0.2),
+            GaussianBlur(p=0.1),
+            Solarization(p=0.2),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225])
+        ])
+
+    def __call__(self, x):
+        return random.choice(
+            [
+                self.transform,
+                self.transform_prime
+            ]
+        )(x)
